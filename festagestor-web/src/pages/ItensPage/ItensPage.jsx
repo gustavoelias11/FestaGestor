978ab794cc74ca92e570307786316e38867eaeab
Gustavo Elias <gustavomanoelelias13@gmail.com>
2026-09-02 19:26:26 -0300

    feat: cria componente ItemCard com formatacao dinamica e botoes de acao

---
+++ festagestor-web/src/pages/ItensPage/ItensPage.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { itemService } from '../../services/itemService';
+
+export default function ItensPage() {
+    
+}
